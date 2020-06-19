422b9965664e97c19a4296cdfa919b3bd4e59571
SimpleNotSimpler <simplenotsimpler@outlook.com>
2020-06-19 07:07:43 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# ModernTable Example Repository
+Repository to demonstrate a minimal example of the [ModernTable library](https://github.com/simplenotsimpler/modern-table).
+
+[ModernTable Documentation](https://simplenotsimpler.github.io/modern-table/)
---
+++ js/app.js
@@ -0,0 +1,43 @@
+
+// Updated relative json link to be compatible with gh-pages
+// https://www.freecodecamp.org/forum/t/solved-relative-links-work-on-locahost-3000-but-not-on-github-pages/230681
+const localJSON = 'data/sample-sales-data.json';
+
+const sampleTable = new ModernTable(
+  'table-container', // tableContainerID
+  'sample-table', // tableID
+  localJSON, // dataURL
+  {
+    tableClasses: 'table table-bordered table-striped table-hover table-sm',
+    tableCaption: 'Sample Sales Data',  
+    searchClasses: 'form-control mb-2 w-25 float-right',
+    colConfig: {
+      'rpt_date': {
+        'format': 'date-us',
+        'dateFrom': 'YYYY-MM-DD',
+        'dateTo': 'MM/DD/YY',
+        'colTitle': 'report date'
+      },
+      'new_customers': {
+        'format': 'number-grouped',
+        'alignment': 'right'
+      },
+      'revenue': {
+        'format': 'currency-us',
+        'alignment': 'right'
+      },
+      'cogs': {
+        'format': 'currency-us',
+        'alignment': 'right'
+      },
+      'profit': {
+        'format': 'currency-us',
+        'alignment': 'right'
+      },
+      'profit_margin': {
+        'format': 'percent',
+        'numDecimals': 1,
+        'alignment': 'right'
+      }
+    }
+  });
